f923abf70ae5eec044e19ccd73f87cb0bb6ac81d
Djailla <bastien.vallet@gmail.com>
2016-01-29 18:29:53 +0100

    Add support of paperclip to copy the generated URL and allow fast paste :) Using pyperclip module

--- netafff_irun.py
+++ netafff_irun.py
@@ -2,9 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import urllib
+try:
+    import pyperclip
+except:
+    print "*** ATTENTION *** : Installer pyperclip"
+    exit(0)
 
 PREFIX = "http://action.metaffiliation.com/trk.php?mclic=P4572B52EEE1191&redir="
 
-input_url = raw_input("URL irun ?\n")
-
-print "\n%s%s" % (PREFIX, urllib.quote_plus(input_url))
+input_url = raw_input("URL irun : ")
+if not "i-run.fr" in input_url:
+    print "*** ATTENTION *** : URL invalide"
+else:
+    generated_link = "%s%s" % (PREFIX, urllib.quote_plus(input_url))
+    print "\nLien copié dans le presse-papier :"
+    print "%s" % generated_link
+    pyperclip.copy(generated_link)
